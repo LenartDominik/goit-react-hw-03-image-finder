ee45b077a81db8f7e83f3e247408ebb9387168b4
Dominik Lenart <dominik.lenart84@gmail.com>
2023-03-29 21:41:29 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,135 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
+import Loader from './Loader/Loader';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      alt: '',
+      largeImg: '',
+      error: false,
+      isLoaded: false,
+      query: '',
+      items: [],
+      API: '33214454-ae922a6a5ae9b73ef58c0c979',
+      PER_PAGE: 12,
+      page: 1,
+    };
+  }
+
+  componentDidMount() {
+    this.delayedFetch();
+    console.log('%c mount', 'color: purple');
+  }
+
+  componentDidUpdate() {
+    console.log('%c update', 'color: blue');
+  }
+
+  delayedFetch() {
+    setTimeout(() => {
+      this.fetchImages();
+    }, 500);
+  }
+  async fetchImages() {
+    const { API, PER_PAGE, page, query } = this.state;
+
+    try {
+      const URL = `https://pixabay.com/api/?key=${API}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`;
+      const res = await axios.get(URL);
+      if (res && res.data.hits.length > 0) {
+        this.setState({
+          isLoaded: true,
+          items: [...this.state.items, ...res.data.hits],
+        });
+      } else {
+        Notify.info(
+          'Sorry, there are no matches found. Try entering another query'
+        );
+      }
+    } catch {
+      this.setState({
+        isLoaded: false,
+        error: true,
+      });
+      console.log('error while fetching results');
+    }
+  }
+
+  handleSubmit = query => {
+    if (query !== this.state.query) {
+      this.setState({
+        isLoaded: false,
+        page: 1,
+        items: [],
+        query: query,
+      });
+      this.delayedFetch();
+    } else {
+      Notify.info('You are already looking at results of this query');
+    }
+  };
+
+  showModal = (url, alt) => {
+    this.setState({
+      showModal: true,
+      largeImg: url,
+      alt: alt,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  loadMore = e => {
+    e.preventDefault();
+    this.setState({
+      isLoaded: false,
+      page: this.state.page + 1,
+    });
+    this.delayedFetch();
+  };
+
+  render() {
+    const { isLoaded, items, PER_PAGE, largeImg, alt, showModal } = this.state;
+
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplatColumns: '1fr',
+          gridGap: 16,
+          paddingBbottom: 24,
+        }}
+      >
+        <Searchbar onSubmit={this.handleSubmit} />
+
+        {!isLoaded && <Loader />}
+        <ImageGallery>
+          <ImageGalleryItem
+            hits={items}
+            onClick={this.showModal}
+          ></ImageGalleryItem>
+        </ImageGallery>
+
+        {items.length > PER_PAGE - 1 && <Button onClick={this.loadMore} />}
+        {showModal && (
+          <Modal image={largeImg} alt={alt} onClose={this.hideModal} />
+        )}
+      </div>
+    );
+  }
+}
