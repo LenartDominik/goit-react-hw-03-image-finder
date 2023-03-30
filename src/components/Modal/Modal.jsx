ae688a2e5c2f298df64d4cece8afc9d9efab34ed
Dominik Lenart <dominik.lenart84@gmail.com>
2023-03-30 12:23:31 +0200

    update

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+import css from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+class Modal extends Component {
+  static propTypes = {
+    image: PropTypes.string,
+    onClose: PropTypes.func,
+    alt: PropTypes.string,
+  };
+
+  componentDidMount() {
+    // console.log('Modal componentDidMount');
+    window.addEventListener('keydown', this.escFunction);
+  }
+
+  componentWillUnmount() {
+    // console.log('Modal componentWillUnmount');
+    window.removeEventListener('keydown', this.escFunction);
+  }
+
+  escFunction = e => {
+    console.log('escape clicked', e);
+    if (e.code === 'Escape') {
+      return this.props.onClose();
+    }
+  };
+  render() {
+    const { image, onClose, alt } = this.props;
+    return (
+      <div className={css.overlay} onClick={onClose}>
+        <div className={css.modal}>
+          <img src={image} alt={alt} />
+        </div>
+      </div>
+    );
+  }
+}
+export default Modal;
